ec36bc251552058fbc20e4fa5c6558f326b6f7e0
seonghJoo <etkl8888@naver.com>
2023-03-20 16:06:45 +0900

    [DEPLOYMENT] useless code

--- code_review.py
+++ code_review.py
@@ -7,9 +7,9 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 # Get the list of changed files
 try:
-    output = subprocess.check_output(["git", "diff", "--name-only", "HEAD^", "HEAD"]).decode("utf-8")
+    output = subprocess.check_output(["git", "diff", "--name-only", "HEAD~1", "HEAD"]).decode("utf-8")
 except subprocess.CalledProcessError:
-    output = subprocess.check_output(["git", "show", "--pretty=format:", "--name-only", "HEAD"]).decode("utf-8")
+    output = subprocess.check_output(["git", "show", "--pretty=format:", "--name-only", "HEAD~1"]).decode("utf-8")
 
 file_paths = output.split("\n")
 
